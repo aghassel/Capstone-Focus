4720dfc4be3d0248ecbbe37a8c6eeb1e67d8afa6
Keshav Aggarwal <19ka47@queensu.ca>
2024-03-13 10:38:08 -0400

    server will stay up

--- host/car_detection/car_detector.py
+++ host/car_detection/car_detector.py
@@ -1,16 +1,22 @@
 import torch
 import cv2
 import numpy as np
-from tracker import EuclideanDistTracker
 import time
+from PIL import Image
 
-class CarDetector:
+
+class ObjectDetector:
     def __init__(self, model_path='ultralytics/yolov5', model_name='yolov5s', device=None):
-        print ("CarDetector __init__")
+        print ("Object Detector Initialization...")
         self.device = device if device else torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.tracker = EuclideanDistTracker()
+        # self.tracker = EuclideanDistTracker()
         self.model = torch.hub.load(model_path, model_name, pretrained=True)
         self.model = self.model.to(self.device)
+        self.class_labels = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        for i in range(len(self.class_labels)):
+            print(f'{i}: {self.class_labels[i]}')
+
+        
         if self.device == 'cuda':
             t = torch.cuda.get_device_properties(0).total_memory
             r = torch.cuda.memory_reserved(0)
@@ -21,31 +27,51 @@ class CarDetector:
             print(f'total memory: {t/1024**3} GB, reserved memory: {r/1024**3} GB, allocated memory: {a/1024**3} GB, free memory: {f/1024**3} GB')
             dummy_out = dummy_out.to('cpu')
 
+    def pred_bbox(self, frame):
 
-    def predict_single_frame(self, frame, threshold=None):
         assert frame is not None, "Frame is None"
-        assert threshold >= 0 and threshold <= 1, "Treshold must be between 0 and 1"
-        
-   
+
         resized_frame = cv2.resize(frame, (640, 480))
-        
+
         results = self.model(resized_frame)
 
-    
-        results = [x for x in results.xyxy[0] if x[5] == 2]
-
-        bbox = [(x[0]*frame.shape[1]/640, x[1]*frame.shape[0]/480, x[2]*frame.shape[1]/640, x[3]*frame.shape[0]/480) for x in results]
-
-        bbox = self.tracker.update(bbox)
-
-        if threshold is not None:
-            threshold_position = int(frame.shape[1] * (1 - threshold))
-            bbox = [i for i in bbox if i[0] < threshold_position]
-            bbox = [(i[0], i[1], threshold_position, i[3]) if i[2] > threshold_position else i for i in bbox]
+        bbox = [(x[0]*frame.shape[1]/640, x[1]*frame.shape[0]/480, x[2]*frame.shape[1]/640, x[3]*frame.shape[0]/480, x[5]) for x in results.xyxy[0]]
 
         return bbox
+    
 
-    def demo(self, cap, fps=30, threshold=None):
+    def warning(self, frame):
+        bbox = self.pred_bbox(frame)
+        bbox.sort(key=lambda x: (x[2] - x[0]) * (x[3] - x[1]), reverse=True)
+        if len(bbox) <= 0:
+            return "No oncoming object detected"
+
+        top3 = bbox[:3]
+
+        warning_string = f""
+        for box in top3:
+            warning_string += f"{self.class_labels[int(box[4])]}:"
+
+            image_center_x = frame.shape[1] / 2
+
+            box_center_x = (box[0] + box[2]) / 2
+
+            position_x = box_center_x - image_center_x
+
+            if position_x < -0.3 * image_center_x or position_x > 0.3 * image_center_x:
+                if position_x < -0.3 * image_center_x:
+                    warning_string += " left\n"
+                else:
+                    warning_string += " right\n"
+            else:
+                warning_string += " center\n"
+
+        return warning_string
+        
+
+    def demo(self, cap, fps=30):
+        assert cap is not None, "Cap is None"
+
         delay = 1 / fps
         frame_count = 0
         time_total = 0
@@ -55,23 +81,48 @@ class CarDetector:
             ret, frame = cap.read()
             if not ret:
                 break
-            
+
             tic = time.time()
-            bbox = self.predict_single_frame(frame, threshold)
+            bbox = self.pred_bbox(frame)
             toc = time.time()
             time_total += toc - tic
             frame_count += 1
 
-            for i in bbox:
-                cv2.rectangle(frame, (int(i[0]), int(i[1])), (int(i[2]), int(i[3])), (0, 255, 0), 2)
-                cv2.putText(frame, 'Car', (int(i[0]), int(i[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            # Sort the bounding boxes by their area in descending order
+            bbox.sort(key=lambda x: (x[2] - x[0]) * (x[3] - x[1]), reverse=True)
 
-            if threshold is not None:
-                threshold_position = int(frame.shape[1] * (1 - threshold))
-                cv2.line(frame, (threshold_position, 0), (threshold_position, frame.shape[0]), (0, 0, 255), 2)
+            # Get the largest bounding boxe
+            if len(bbox) <= 0:
+                print ("No oncoming object detected")
+                continue
+            box = bbox[0]
+
+            # Calculate the center of the image
+            image_center_x = frame.shape[1] / 2
+            image_center_y = frame.shape[0] / 2
+
+            # Calculate the position of each bounding box in relation to the center of the image
+            warning_string = f"{self.class_labels[int(box[4])]} "
+            box_center_x = (box[0] + box[2]) / 2
+            box_center_y = (box[1] + box[3]) / 2
+            position_x = box_center_x - image_center_x
+            position_x = box_center_x - image_center_x
+            if position_x < -0.3 * image_center_x or position_x > 0.3 * image_center_x:
+                if position_x < -0.3 * image_center_x:
+                    warning_string += "to your left"        
+                else:
+                    warning_string += "to your right"
+            else:
+                warning_string += "in front of you"
+           
+            
+            print (warning_string)
+
+            cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 255, 255), 2)
+            cv2.putText(frame, warning_string, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
             cv2.imshow('Frame', frame)
-
+           
             # Calculate the time taken to process the frame
             time_taken = time.time() - start_time
 
@@ -87,11 +138,11 @@ class CarDetector:
         print(f'Average time taken to process a frame: {(time_total/frame_count):.2f}ms')
         cap.release()
         cv2.destroyAllWindows()
-
-
+    
+        
 if __name__ == "__main__":
-    cap = cv2.VideoCapture('test_data/cars.mp4')
+    cap = cv2.VideoCapture('test_data/fullstreet.mp4')
     fps = 30
-    detector = CarDetector()
-    detector.demo(cap=cap, fps=fps, threshold=0.4)
+    detector = ObjectDetector()
+    detector.demo(cap=cap, fps=fps)
     cap.release()
--- host/main.py
+++ host/main.py
@@ -1,23 +1,18 @@
 import socket
 import cv2
 import numpy as np
-import struct
-from car_detection.car_detector import CarDetector
-
-
-detector = CarDetector()
-
-
-def process_car(frame):
-    bbox = detector.predict_single_frame(frame, threshold=0.5)
-    return bbox
-
+import struct, io
+from car_detection.car_detector import ObjectDetector
 
 from asl import process_asl, init_asl
 
 init_asl()
 
+detector = ObjectDetector()
 
+# def process_car(frame, threshold):
+#     bbox = detector.predict_single_frame(frame, threshold)
+#     return bbox
 
 HOST = ''
 PORT = 8000
@@ -27,47 +22,59 @@ server_socket.bind((HOST, PORT))
 server_socket.listen(1)
 print(f"Listening on port {PORT}...")
 
-conn, addr = server_socket.accept()
-print(f"Connected by {addr}")
-
-try:
-    while True:
-        # Read and process messages based on their type
-        header = conn.recv(5)  # Read the message type and length
-        if not header:
-            print("No header received, closing connection.")
-            break
-        message_type, message_length = struct.unpack('<BI', header)
-        
-        message_data = b""
-        while len(message_data) < message_length:
-            packet = conn.recv(message_length - len(message_data))
-            if not packet:
+while True:
+    conn, addr = server_socket.accept()
+    print(f"Connected by {addr}")
+    run_server = True
+    try:
+        while run_server:
+            # Read and process messages based on their type
+            header = conn.recv(5)  # Read the message type and length
+            # if addrfrom != addr:
+            #     print(addrfrom, addr)
+            #     continue
+            if not header :
+                print("No header received, closing connection.")
+                # continue
+                run_server = False
+                conn.close()
                 break
-            message_data += packet
+            message_type, message_length = struct.unpack('<BI', header)
+            
+            message_data = b""
+            while len(message_data) < message_length:
+                packet = conn.recv(message_length - len(message_data))
+                if not packet:
+                    break
+                message_data += packet
 
-        if message_type == 0:  # Mode message
-            mode = message_data.decode('utf-8')
-        elif message_type == 1:  # Image data
-            frame_data = np.frombuffer(message_data, dtype=np.uint8)
-            frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
-            if frame is not None:
-                if mode == "ASL":
-                    label, confidence = process_asl(frame)
-                    # Ensure label and confidence are not None
-                    label = label if label is not None else "Unknown"
-                    confidence = confidence if confidence is not None else 0.0
-                    response = f"{label},{confidence:.2f}".encode('utf-8')
-                elif mode == "CAR":
-                    bbox = process_car(frame)
-                    response += f"{len(bbox)}".encode('utf-8')
-                    for i in bbox:
-                        response += f",{i[0]},{i[1]},{i[2]},{i[3]}".encode('utf-8')   
-                    print (response)               
-         
-                conn.sendall(response)
-            else:
-                print("Failed to decode frame.")
-finally:
-    conn.close()
-    server_socket.close()
+            if message_type == 0:  # Mode message
+                mode = message_data.decode('utf-8')
+            elif message_type == 1:  # Image data
+                frame_data = np.frombuffer(message_data, dtype=np.uint8)
+                frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
+                if frame is not None:
+                    if mode == "ASL":
+                        label, confidence = process_asl(frame)
+                            # Ensure label and confidence are not None
+                        label = label if label is not None else "Unknown"
+                        confidence = confidence if confidence is not None else 0.0
+
+                        response = f"{label},{confidence:.2f}".encode('utf-8')
+                        conn.sendall(response)
+                    elif mode == "CAR":
+                        # response = ''
+
+                        warning = detector.warning(frame)
+                        # print("sending")
+                        conn.sendto(warning.encode('utf-8'), addr)
+
+        
+                else:
+                    print("Failed to decode frame.")
+    finally:
+        pass
+    # conn.close()
+    # server_socket.close()
+# conn.close()
+# server_socket.close()
